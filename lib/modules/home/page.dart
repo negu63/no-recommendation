cce90abe25812096f78fa8bb4dedeba64d591d2b
negu63 <dnjsdn2019@gmail.com>
2021-10-24 00:32:50 +0900

    feat: 홈 페이지 검색창 추가

--- lib/modules/home/page.dart
+++ lib/modules/home/page.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:norecommendation/core/utils/responsive.dart';
+import 'package:norecommendation/modules/home/controller.dart';
+import 'package:norecommendation/modules/home/local_widgets/search_input.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final SearchInputController searchInputController =
+        Get.put(SearchInputController());
+
+    return Scaffold(
+      body: Responsive(
+        mobile: Column(
+          children: [
+            SearchInput(),
+          ],
+        ),
+        tablet: Container(),
+        desktop: Container(),
+      ),
+    );
   }
 }
