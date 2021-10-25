e9270f82be4551996f2cb927fc07817cb4370225
negu63 <dnjsdn2019@gmail.com>
2021-10-25 21:50:05 +0900

    feat: 페이지 클래스 웹뷰 추가

--- lib/routes/pages.dart
+++ lib/routes/pages.dart
@@ -1,10 +1,12 @@
 import 'package:get/get.dart';
 import 'package:norecommendation/modules/home/page.dart';
+import 'package:norecommendation/modules/web/page.dart';
 
 import 'routes.dart';
 
 class AppPages {
   static final pages = [
     GetPage(name: Routes.HOME, page: () => HomePage()),
+    GetPage(name: Routes.WEB, page: () => WebPage()),
   ];
 }
