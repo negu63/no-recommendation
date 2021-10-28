5e711cb1bb47abe9a812763f76a06bbbf69fe367
negu63 <dnjsdn2019@gmail.com>
2021-10-29 00:57:04 +0900

    feat: 홈 페이지 검색창 focusOut 감지 및 처리 추가

--- lib/modules/home/page.dart
+++ lib/modules/home/page.dart
@@ -3,20 +3,31 @@ import 'package:get/get.dart';
 import 'package:norecommendation/core/utils/responsive.dart';
 import 'package:norecommendation/modules/home/controller.dart';
 import 'package:norecommendation/modules/home/local_widgets/search_input.dart';
+import 'package:norecommendation/modules/web/controller.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    Get.put(SearchInputController());
+    final SearchInputController searchInputController =
+        Get.put(SearchInputController());
+    final WebViewController webViewController = Get.put(WebViewController());
 
     return Scaffold(
       body: Responsive(
-        mobile: Column(
-          children: [
-            SearchInput(),
-          ],
+        mobile: GestureDetector(
+          behavior: HitTestBehavior.opaque,
+          onTap: () {
+            searchInputController.focusOut();
+            FocusScope.of(context).unfocus();
+          },
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SearchInput(),
+            ],
+          ),
         ),
         tablet: Container(),
         desktop: Container(),
