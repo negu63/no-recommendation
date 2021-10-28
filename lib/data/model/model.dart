153c8f2de81cc3342a57a20d76f13ed6398e30d1
negu63 <dnjsdn2019@gmail.com>
2021-10-28 23:29:28 +0900

    feat: History 모델 추가

---
+++ lib/data/model/model.dart
@@ -0,0 +1,17 @@
+import 'package:hive/hive.dart';
+
+part 'model.g.dart';
+
+@HiveType(typeId: 0)
+class History extends HiveObject {
+  @HiveField(0)
+  String title;
+
+  @HiveField(1)
+  String url;
+
+  @HiveField(2)
+  String date;
+
+  History(this.title, this.url, this.date);
+}
