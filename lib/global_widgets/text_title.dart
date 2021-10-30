8e4cedd8011eb4da5f9eec38ce3f3788d034db7c
negu63 <dnjsdn2019@gmail.com>
2021-10-31 02:20:56 +0900

    feat: 텍스트 타이틀 위젯 추가

---
+++ lib/global_widgets/text_title.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TextTitle extends StatelessWidget {
+  const TextTitle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      "NoThankYou",
+      // style: Theme.of(context).textTheme.headline3,
+    );
+  }
+}
