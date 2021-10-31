48f722df66b10388580d1e6579522a6bc9af17f4
negu63 <dnjsdn2019@gmail.com>
2021-10-31 16:54:18 +0900

    feat: 검색 페이지 검색 기록 위젯 추가

---
+++ lib/modules/search/local_widgets/search_history.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:norecommendation/modules/search/controller.dart';
+
+class SearchHistory extends StatelessWidget {
+  const SearchHistory({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final SearchInputController searchInputController = Get.find();
+
+    return Obx(
+      () => ListView.builder(
+          shrinkWrap: true,
+          physics: const BouncingScrollPhysics(),
+          itemCount: searchInputController.queries.length,
+          itemBuilder: (BuildContext context, int index) {
+            int reverseIndex = searchInputController.queries.length - 1 - index;
+            return InkWell(
+              onTap: () {
+                searchInputController.searchByHistory(reverseIndex);
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(searchInputController.queries[reverseIndex]),
+                  IconButton(
+                    onPressed: () {
+                      searchInputController.removeAtQuery(reverseIndex);
+                    },
+                    icon: const Icon(Icons.clear),
+                  ),
+                ],
+              ),
+            );
+          }),
+    );
+  }
+}
