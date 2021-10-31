3e5a22f9f36e4113e954738d5c99ef7a8235672b
negu63 <dnjsdn2019@gmail.com>
2021-10-31 15:51:53 +0900

    feat: 페이지 클래스 검색 페이지 추가

--- lib/routes/pages.dart
+++ lib/routes/pages.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 import 'package:norecommendation/modules/home/page.dart';
+import 'package:norecommendation/modules/search/page.dart';
 import 'package:norecommendation/modules/web/page.dart';
 
 import 'routes.dart';
@@ -8,5 +9,6 @@ class AppPages {
   static final pages = [
     GetPage(name: Routes.HOME, page: () => HomePage()),
     GetPage(name: Routes.WEB, page: () => WebPage()),
+    GetPage(name: Routes.SEARCH, page: () => SearchPage()),
   ];
 }
