f4e6bacd9fa07b6b5fed44743b76e7283bfc814e
negu63 <dnjsdn2019@gmail.com>
2021-11-01 00:25:32 +0900

    feat: 설정 페이지 추가

---
+++ lib/modules/setting/page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:norecommendation/modules/setting/controller.dart';
+import 'package:norecommendation/modules/setting/local_widgets/setting_appbar.dart';
+import 'package:norecommendation/modules/setting/local_widgets/setting_subtitle.dart';
+import 'package:norecommendation/modules/setting/local_widgets/toggle_button.dart';
+
+class SettingPage extends StatelessWidget {
+  const SettingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final SettingController settingController = Get.find();
+
+    return Scaffold(
+      appBar: SettingAppBar(
+        appbar: AppBar(),
+      ),
+      body: ListView(
+        physics: const BouncingScrollPhysics(),
+        shrinkWrap: true,
+        children: [
+          SettingSubtitle(),
+          ToggleButton(
+            icon: Icons.search,
+            title: '검색 기록',
+            name: 'query',
+            state: settingController.savingQuery,
+          ),
+          Divider(),
+        ],
+      ),
+    );
+  }
+}
