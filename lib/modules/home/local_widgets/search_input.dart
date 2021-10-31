ef73d57a70da1f92f80528807a54d3809c78fc35
negu63 <dnjsdn2019@gmail.com>
2021-10-31 15:55:48 +0900

    feat: 홈 페이지 검색창 기능 변경

--- lib/modules/home/local_widgets/search_input.dart
+++ lib/modules/home/local_widgets/search_input.dart
@@ -1,35 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_utils/src/extensions/context_extensions.dart';
-import 'package:norecommendation/modules/home/controller.dart';
 
 class SearchInput extends StatelessWidget {
   const SearchInput({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    final TextEditingController textEditingController = TextEditingController();
-    final SearchInputController searchInputController = Get.find();
-
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        SizedBox(
-          width: context.width * (2 / 3),
-          child: TextField(
-            controller: textEditingController,
-            decoration: InputDecoration(
-              suffixIcon: IconButton(
-                onPressed: () {
-                  searchInputController.search(textEditingController.text);
-                },
-                icon: const Icon(Icons.search),
+        GestureDetector(
+          onTap: () {
+            Get.toNamed('/search');
+          },
+          child: SizedBox(
+            width: context.width * (2 / 3),
+            child: const TextField(
+              decoration: InputDecoration(
+                suffixIcon: Icon(Icons.search),
               ),
+              enabled: false,
             ),
-            onTap: () {
-              debugPrint('textfield');
-              searchInputController.focusIn();
-            },
           ),
         ),
       ],
