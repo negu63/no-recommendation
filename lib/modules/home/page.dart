97db5b34b1699784de2066e6dc53e85bbd15ce70
negu63 <dnjsdn2019@gmail.com>
2021-10-31 15:46:32 +0900

    feat: 홈 페이지 검색 기록 제거

--- lib/modules/home/page.dart
+++ lib/modules/home/page.dart
@@ -3,8 +3,6 @@ import 'package:get/get.dart';
 import 'package:norecommendation/core/utils/responsive.dart';
 import 'package:norecommendation/global_widgets/menu_drawer.dart';
 import 'package:norecommendation/global_widgets/text_title.dart';
-import 'package:norecommendation/modules/home/controller.dart';
-import 'package:norecommendation/modules/home/local_widgets/search_history.dart';
 import 'package:norecommendation/modules/home/local_widgets/search_input.dart';
 import 'package:norecommendation/modules/web/controller.dart';
 
@@ -14,48 +12,34 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-    final SearchInputController searchInputController =
-        Get.put(SearchInputController());
-    final WebViewController webViewController = Get.put(WebViewController());
+    Get.put(WebViewController());
 
     return Scaffold(
       key: _scaffoldKey,
       drawer: MenuDrawer(),
       body: Responsive(
-        mobile: GestureDetector(
-          behavior: HitTestBehavior.opaque,
-          onTap: () {
-            searchInputController.focusOut();
-            FocusScope.of(context).unfocus();
-          },
-          child: Stack(
-            children: [
-              Positioned(
-                top: 30,
-                left: 15,
-                child: IconButton(
-                  icon: Icon(Icons.menu),
-                  iconSize: 30,
-                  color: Colors.blue,
-                  onPressed: () {
-                    _scaffoldKey.currentState!.openDrawer();
-                  },
-                ),
+        mobile: Stack(
+          children: [
+            Positioned(
+              top: 30,
+              left: 15,
+              child: IconButton(
+                icon: Icon(Icons.menu),
+                iconSize: 30,
+                color: Colors.blue,
+                onPressed: () {
+                  _scaffoldKey.currentState!.openDrawer();
+                },
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextTitle(),
-                  SearchInput(),
-                  Obx(
-                    () => searchInputController.isFocused.value == true
-                        ? SearchHistory()
-                        : Container(),
-                  ),
-                ],
-              ),
-            ],
-          ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextTitle(),
+                SearchInput(),
+              ],
+            ),
+          ],
         ),
         tablet: Container(),
         desktop: Container(),
