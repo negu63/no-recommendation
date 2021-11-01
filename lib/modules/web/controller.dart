0646a7bdc3ecdf1f817e1d5ac97ea35acdc6a3a1
negu63 <dnjsdn2019@gmail.com>
2021-11-01 23:53:10 +0900

    refactor: 웹뷰 페이지 컨트롤러 박스매니저 적용

--- lib/modules/web/controller.dart
+++ lib/modules/web/controller.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:norecommendation/core/utils/box_manager.dart';
 import 'package:norecommendation/data/model/model.dart';
 
 class WebViewController extends GetxController {
@@ -18,21 +19,13 @@ class WebViewController extends GetxController {
   }
 
   Future getHistories() async {
-    Box box;
-    try {
-      box = Hive.box('db');
-    } catch (error) {
-      box = await Hive.openBox('db');
-      debugPrint(error.toString());
-    }
-
-    var _histories = box.get('histories');
+    var _histories = loadFromHive('db', 'histories');
     if (_histories != null) histories.value = _histories;
   }
 
   void addHistory(History history) async {
     histories.add(history);
-    Box box = await Hive.openBox('db');
-    box.put('histories', histories);
+
+    saveToHive('db', 'histories', histories);
   }
 }
