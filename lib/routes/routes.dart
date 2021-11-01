a255cb48c089e56eb453b22e630dcac5c4b3af8e
negu63 <dnjsdn2019@gmail.com>
2021-11-01 23:34:05 +0900

    feat: 시청 기록 페이지 라우트 추가

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,7 +1,10 @@
+import 'package:norecommendation/data/model/model.dart';
+
 class Routes {
   // final SPLASH = '';
   static const HOME = '/';
   static const WEB = '/web/:query';
   static const SEARCH = '/search';
   static const SETTING = '/setting';
+  static const HISTORY = '/history';
 }
