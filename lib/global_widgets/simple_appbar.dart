2b0a5d682cc7f939080a482146d75dd7d589ba0d
negu63 <dnjsdn2019@gmail.com>
2021-11-01 23:40:44 +0900

    feat: 심플 앱바 위젯 추가

---
+++ lib/global_widgets/simple_appbar.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SimpleAppbar extends StatelessWidget implements PreferredSizeWidget {
+  const SimpleAppbar({
+    Key? key,
+    required this.appbar,
+    required this.title,
+    required this.iconColor,
+    required this.foreColor,
+    required this.backgroundColor,
+    required this.shadowColor,
+  }) : super(key: key);
+
+  final AppBar appbar;
+  final String title;
+  final Color iconColor;
+  final Color foreColor;
+  final Color backgroundColor;
+  final Color shadowColor;
+
+  @override
+  Size get preferredSize => appbar.preferredSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: IconButton(
+        onPressed: () {
+          Get.back();
+        },
+        icon: Icon(
+          Icons.arrow_back,
+          color: iconColor,
+        ),
+      ),
+      title: Text(
+        title,
+        style: TextStyle(color: foreColor),
+      ),
+      backgroundColor: backgroundColor,
+      shadowColor: shadowColor,
+    );
+  }
+}
