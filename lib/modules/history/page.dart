055c281823d3df59a8a61530425dcf9e7eca0e13
negu63 <dnjsdn2019@gmail.com>
2021-11-02 00:35:19 +0900

    feat: 시청 기록 페이지 컨트롤러 연동

--- lib/modules/history/page.dart
+++ lib/modules/history/page.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:norecommendation/data/model/model.dart';
 import 'package:norecommendation/global_widgets/simple_appbar.dart';
+import 'package:norecommendation/modules/history/controller.dart';
+import 'package:norecommendation/modules/history/local_widgets/watch_history.dart';
 
 class HistoryPage extends StatelessWidget {
   const HistoryPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final HistoryController historyController = Get.find();
+
     return Scaffold(
       appBar: SimpleAppbar(
         appbar: AppBar(),
@@ -15,7 +21,19 @@ class HistoryPage extends StatelessWidget {
         backgroundColor: Theme.of(context).canvasColor,
         shadowColor: Colors.transparent,
       ),
-      body: Text('history page'),
+      body: Obx(
+        () => ListView.builder(
+          itemCount: historyController.histories.length,
+          itemBuilder: (BuildContext context, int index) {
+            int reverseIndex = historyController.histories.length - 1 - index;
+            History history = historyController.histories[reverseIndex];
+            return InkWell(
+              onTap: () {},
+              child: WatchHistory(history: history),
+            );
+          },
+        ),
+      ),
     );
   }
 }
