f08ad57891fccc6eee6eda31dc12fe047475cf95
negu63 <dnjsdn2019@gmail.com>
2021-11-01 23:43:23 +0900

    feat: 시청 기록 페이지 심플 앱바 추가

--- lib/modules/history/page.dart
+++ lib/modules/history/page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:norecommendation/global_widgets/simple_appbar.dart';
 
 class HistoryPage extends StatelessWidget {
   const HistoryPage({Key? key}) : super(key: key);
@@ -6,6 +7,14 @@ class HistoryPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: SimpleAppbar(
+        appbar: AppBar(),
+        title: 'History',
+        iconColor: Colors.black,
+        foreColor: Colors.black,
+        backgroundColor: Theme.of(context).canvasColor,
+        shadowColor: Colors.transparent,
+      ),
       body: Text('history page'),
     );
   }
