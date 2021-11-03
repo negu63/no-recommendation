21dafb63cd49c8f0cd105a9951d8a35dce8073a5
negu63 <dnjsdn2019@gmail.com>
2021-11-04 01:31:13 +0900

    refactor: 검색, 시청 라우트 분리

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -3,7 +3,8 @@ import 'package:norecommendation/data/model/model.dart';
 class Routes {
   // final SPLASH = '';
   static const HOME = '/';
-  static const WEB = '/web/:query';
+  static const WEB_SEARCH = '/web/search/:query';
+  static const WEB_WATCH = '/web/watch/:videoId';
   static const SEARCH = '/search';
   static const SETTING = '/setting';
   static const HISTORY = '/history';
