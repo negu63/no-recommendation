a9d1bf3eea8a2e41a5c4cc783534f9ccab9662fc
negu63 <dnjsdn2019@gmail.com>
2021-11-05 00:12:44 +0900

    feat: 설정 페이지 부제목 위젯 변수로 받게 변경

--- lib/modules/setting/local_widgets/setting_subtitle.dart
+++ lib/modules/setting/local_widgets/setting_subtitle.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 
 class SettingSubtitle extends StatelessWidget {
-  const SettingSubtitle({Key? key}) : super(key: key);
+  const SettingSubtitle({Key? key, required this.title}) : super(key: key);
+
+  final String title;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.fromLTRB(24, 16, 8, 8),
       child: Text(
-        'subtitle',
+        title,
         style: Theme.of(context).textTheme.subtitle1!.copyWith(
               fontWeight: FontWeight.bold,
               color: Theme.of(context).primaryColor,
