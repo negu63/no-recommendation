0bd44db96ab8ea162b8d4deeaf914265ee1b8c60
negu63 <dnjsdn2019@gmail.com>
2021-11-11 00:18:14 +0900

    feat: 반응형 유틸 safearea 추가

--- lib/core/utils/responsive.dart
+++ lib/core/utils/responsive.dart
@@ -24,16 +24,18 @@ class Responsive extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        if (constraints.maxWidth >= 1100) {
-          return desktop;
-        } else if (constraints.maxWidth >= 650) {
-          return tablet;
-        } else {
-          return mobile;
-        }
-      },
+    return SafeArea(
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth >= 1100) {
+            return desktop;
+          } else if (constraints.maxWidth >= 650) {
+            return tablet;
+          } else {
+            return mobile;
+          }
+        },
+      ),
     );
   }
 }
