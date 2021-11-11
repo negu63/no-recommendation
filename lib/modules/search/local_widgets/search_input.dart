89685948f5962a4b026b2c64b92a2a3b892b231b
negu63 <dnjsdn2019@gmail.com>
2021-11-11 22:26:30 +0900

    feat: 검색 페이지 검색 창 위젯 디자인

--- lib/modules/search/local_widgets/search_input.dart
+++ lib/modules/search/local_widgets/search_input.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:norecommendation/core/theme/color_theme.dart';
 import 'package:norecommendation/core/utils/space_replacer.dart';
 import 'package:norecommendation/modules/search/controller.dart';
 import 'package:norecommendation/modules/setting/controller.dart';
@@ -11,25 +12,41 @@ class SearchInput extends StatelessWidget {
     final TextEditingController textEditingController = TextEditingController();
     final SearchInputController searchInputController = Get.find();
     final SettingController settingController = Get.find();
+    const border = UnderlineInputBorder(
+      borderSide: BorderSide(
+        color: Colors.transparent,
+      ),
+    );
 
     return SizedBox(
       width: Get.width,
       child: TextField(
+        cursorColor: darkGrey18,
         controller: textEditingController,
         decoration: InputDecoration(
           prefixIcon: IconButton(
               onPressed: () {
                 Get.back();
               },
-              icon: const Icon(Icons.arrow_back)),
+              icon: const Icon(
+                Icons.arrow_back,
+                color: darkGrey18,
+              )),
           suffixIcon: IconButton(
             onPressed: () {
               searchInputController.search(
                   replaceStringSpace(textEditingController.text),
                   settingController.savingQuery.value);
             },
-            icon: const Icon(Icons.search),
+            icon: const Icon(
+              Icons.search,
+              color: darkGrey18,
+            ),
           ),
+          border: border,
+          enabledBorder: border,
+          focusedBorder: border,
+          disabledBorder: border,
         ),
         onSubmitted: (value) {
           searchInputController.search(
