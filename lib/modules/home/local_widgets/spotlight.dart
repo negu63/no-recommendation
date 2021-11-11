20cdab00f756e3508f753ea060b83a8d97805aed
negu63 <dnjsdn2019@gmail.com>
2021-11-11 17:26:40 +0900

    feat: spotlight 위젯 path 수정

--- lib/modules/home/local_widgets/spotlight.dart
+++ lib/modules/home/local_widgets/spotlight.dart
@@ -1,21 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:norecommendation/core/theme/color_theme.dart';
 
 class Spotlight extends StatelessWidget {
   const Spotlight({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Stack(
-        children: [
-          CustomPaint(
-            size: Size(Get.width, Get.height),
-            painter: DrawTriangle(),
-          ),
-        ],
-      ),
+    return Stack(
+      children: [
+        CustomPaint(
+          size: Size(Get.width, Get.height),
+          painter: DrawTriangle(),
+        ),
+      ],
     );
   }
 }
@@ -25,16 +22,11 @@ class DrawTriangle extends CustomPainter {
   void paint(Canvas canvas, Size size) {
     var path = Path();
     path.moveTo(size.width / 2, 0);
-    path.lineTo(40, size.height / 1.9);
-    path.cubicTo(
-        size.width / 5,
-        size.height / 1.55,
-        size.width - (size.width / 5),
-        size.height / 1.55,
-        size.width - 40,
-        size.height / 1.9);
-
-    canvas.drawPath(path, Paint()..color = Color.fromRGBO(255, 255, 255, 1));
+    path.lineTo(0, size.height / 1.5);
+    path.lineTo(0, size.height);
+    path.lineTo(size.width, size.height);
+    path.lineTo(size.width, size.height / 1.5);
+    canvas.drawPath(path, Paint()..color = Colors.white);
   }
 
   @override
