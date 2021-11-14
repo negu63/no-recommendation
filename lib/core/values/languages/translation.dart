57438df8bbf16b216c35101c57decbb09f7984ff
negu63 <dnjsdn2019@gmail.com>
2021-11-14 22:51:48 +0900

    feat: 번역 텍스트 파일 추가

---
+++ lib/core/values/languages/translation.dart
@@ -0,0 +1,13 @@
+import 'package:get/get.dart';
+
+class Messages extends Translations {
+  @override
+  Map<String, Map<String, String>> get keys => {
+        'en_US': {
+          'settingTitle': 'Setting',
+        },
+        'ko_KR': {
+          'settingTitle': '설정',
+        },
+      };
+}
