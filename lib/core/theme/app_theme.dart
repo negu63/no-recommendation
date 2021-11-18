b3caa542180c63acc6dd4f38a804db9696702682
negu63 <dnjsdn2019@gmail.com>
2021-11-18 19:13:08 +0900

    chore: 앱 테마 다크 모드 추가 및 수정

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,3 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:norecommendation/core/theme/color_theme.dart';
 
-final appThemeData = ThemeData();
+final appLightThemeData = ThemeData.light().copyWith(
+  primaryColor: Colors.white,
+  toggleableActiveColor: darkGrey46,
+);
+final appDarkThemeData = ThemeData.dark().copyWith(
+  primaryColor: darkGrey46,
+);
