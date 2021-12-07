fbd5aa140c7fb0bd4029847c949dd48841f8f944
negu63 <dnjsdn2019@gmail.com>
2021-12-07 18:25:26 +0900

    build: v1.1.1+5

--- lib/modules/oss/page.dart
+++ lib/modules/oss/page.dart
@@ -22,7 +22,7 @@ class OssPage extends StatelessWidget {
       ),
       child: LicensePage(
         applicationName: 'appTitle'.tr,
-        applicationVersion: '1.1.0+4',
+        applicationVersion: '1.1.1+5',
         applicationIcon: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Card(
