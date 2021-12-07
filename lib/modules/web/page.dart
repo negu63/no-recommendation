9b9242194abcebe81666bb254a234cad6cc2b92c
negu63 <dnjsdn2019@gmail.com>
2021-12-07 17:56:02 +0900

    feat: 웹 페이지 옵저버 추가 및 백그라운드 재생 설정 반영 기능 추가

--- lib/modules/web/page.dart
+++ lib/modules/web/page.dart
@@ -2,21 +2,54 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:norecommendation/core/utils/box_manager.dart';
 import 'package:norecommendation/modules/search/controller.dart';
 import 'package:norecommendation/modules/setting/controller.dart';
 
 late InAppWebViewController inAppWebViewController;
+late bool backgroundPlay;
 
-class WebPage extends StatelessWidget {
+class WebPage extends StatefulWidget {
   const WebPage({Key? key, required this.isSearch}) : super(key: key);
 
   final bool isSearch;
 
   @override
+  State<WebPage> createState() => _WebPageState();
+}
+
+class _WebPageState extends State<WebPage> with WidgetsBindingObserver {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance!.addObserver(this);
+    WidgetsBinding.instance!.addPostFrameCallback((_) async {
+      backgroundPlay = await loadFromHive('setting', 'backgroundPlay');
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    WidgetsBinding.instance!.removeObserver(this);
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.inactive && !backgroundPlay) {
+      inAppWebViewController.evaluateJavascript(
+        source:
+            '''var button = document.querySelector('button.icon-button[aria-pressed="false"]');
+            if(button !== null) { button.click(); };''',
+      );
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     final SettingController settingController = Get.find();
     final SearchInputController searchInputController = Get.find();
-    final url = isSearch
+    final url = widget.isSearch
         ? 'https://youtube.com/results?search_query=${searchInputController.query}'
         : 'https://m.youtube.com/feed/library';
 
